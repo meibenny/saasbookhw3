6d2df237bbcc6e2351c91c07211f5b872f51e176
Benny Mei <meibenny@gmail.com>
2015-03-02 20:09:17 -0400

    added stuff to sort movie list

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -14,7 +14,10 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  assert false, "Unimplemented"
+  x = page.body.split(e1)
+  assert x.length == 3
+
 end
 
 # Make it easier to express checking or unchecking several boxes at once
