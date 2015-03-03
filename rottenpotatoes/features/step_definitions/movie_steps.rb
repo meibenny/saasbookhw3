512bbf22b5c0d88f7e3c6f45a8df6da1bde2dec1
Benny Mei <meibenny@gmail.com>
2015-03-02 20:37:34 -0400

    added to movie_steps.rb

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +15,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  assert false, "Unimplemented"
+  #assert false, "Unimplemented"
   x = page.body.split(e1)
   assert x.length == 3
   x = x[2].split(e2)
@@ -29,7 +30,15 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
+  rating_list.split(%r{\s*,\s*}).each_with_index{
+    |name, index|
+      if(!index)
+        When %Q{I #{uncheck}check "ratings[#{name}]"}
+      else
+        And %Q{I #{uncheck}check "ratings[#{name}]"}
+      end
+    }
 end
 
 Then /I should see all the movies/ do
